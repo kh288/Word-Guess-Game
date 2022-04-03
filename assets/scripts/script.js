6069b3e83370cbf8217dd8bd771c50738d5bf3c7
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-03 09:32:25 -0700

    Hide button after starting timer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,35 @@
 var results = document.querySelector("#results");
 var wins = document.querySelector("#wins");
 var losses = document.querySelector("#losses");
-var userInput = document.querySelector("#text-input");
+
 var timer = document.querySelector("#timer");
+
+var userInput = document.querySelector("#text-input");
 var startButton = document.querySelector("#start");
 
 var countdown = 11;
 
 // Timer function
 startButton.addEventListener("click", function(event) {
-    // prevents page refreshing
+    // prevents page refreshing and bubbling
     event.preventDefault();
+    // startButton HIDE BUTTON ON PRESS
+    startButton.setAttribute("style", "visibility: hidden");
+
     // function to change the number as it counts down
-    var timerInterval = setInterval(function() {
+    var counter = setInterval(function() {
         // Subtract 1 from timer, then change the current timer
         countdown--;
+        console.log("Countdown: " + countdown);
         timer.textContent = countdown;
         // if countdown hits 0, stop timer from continueing to subtract past 0
         if(countdown === 0) {
-            clearInterval(timerInterval);
-            console.log("Timer Reached 0");
+            clearInterval(counter);
+            console.log("Timer Reached 0, game done");
+            // Show startButton again on completion of function
+            startButton.setAttribute("style", "visibility: visible");
+            // resets time back to where it should be.
+            countdown = 11;
         }
     }, 1000);
 });
