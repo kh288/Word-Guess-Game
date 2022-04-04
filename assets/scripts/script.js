f3c9a68fabbd5e14df27d7f046448438e923bed6
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-04 09:52:03 -0700

    Add Win loss scoreboard functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,19 @@
 var results = document.querySelector("#results");
-var wins = document.querySelector("#wins");
-var losses = document.querySelector("#losses");
+var winScore = document.querySelector("#wins");
+var lossScore = document.querySelector("#losses");
+var scoreStatus = document.querySelector("#score-status");
+
+var wins = 0;
+if (localStorage.getItem("wins")) {
+    wins = JSON.parse(localStorage.getItem("wins"));
+    winScore.textContent = localStorage.getItem("wins", wins);
+}
+
+var loss = 0;
+if (localStorage.getItem("loss")) {
+    loss = JSON.parse(localStorage.getItem("loss"));
+    lossScore.textContent = localStorage.getItem("loss", loss);
+}
 
 var timer = document.querySelector("#timer");
 
@@ -8,7 +21,7 @@ var userInput = document.querySelector("#text-input");
 var userForm = document.querySelector("#input-form");
 var startButton = document.querySelector("#start");
 
-var countdown = 11;
+var countdown = 10;
 var inputArray = [];
 var hiddenWord =[];
 var currentGuess = "";
@@ -19,7 +32,9 @@ var wordLibrary =
 "singer",
 "foreign",
 "height",
-"chunky"];
+"chunky",
+"flavor",
+"indigo"];
 
 // Timer function
 function startTimer(event) {
@@ -34,22 +49,39 @@ function startTimer(event) {
     var counter = setInterval(function() {
         // textContent ACTUALLY changes the text for the timer
         timer.textContent = countdown;
-        console.log(localStorage.getItem("CurrentWord"));
+        // console.log(localStorage.getItem("CurrentWord"));
 
+        // WIN
         if (currentGuess == currentWord) {
             console.log("Win!");
+            results.setAttribute("style", "color: green");
+            wins++;
+            localStorage.setItem("wins", wins);
+            winScore.textContent = localStorage.getItem("wins", wins);
             countdown = 0;
+            scoreStatus.textContent = "You win!";
+            scoreStatus.setAttribute("style", "color: green");
         }
 
         // if countdown hits 0, stop timer from continueing to subtract past 0
         if(countdown === 0) {
+            // LOSS
+            if (currentGuess != currentWord) {
+                results.setAttribute("style", "color: red");
+                loss++;
+                localStorage.setItem("loss", loss);
+                lossScore.textContent = localStorage.getItem("wins", loss);
+                scoreStatus.textContent = "You lost!";
+                scoreStatus.setAttribute("style", "color: red");
+            }
             clearInterval(counter);
             console.log("Timer Reached 0, game done");
             // Show startButton again on completion of function
             startButton.setAttribute("style", "display: initial");
             userInput.setAttribute("style", "display: none");
             // resets time back to where it should be.
-            countdown = 11;
+            countdown = 10;
+            return;
         }
         // Subtract 1 from timer, then change the current timer
         countdown--;
@@ -72,6 +104,7 @@ function drawResult() {
     }
 
     // join removes the space and stores it into a string from an array
+    results.setAttribute("style", "#ccc");
     var wordResult = hiddenWord.join(" ");
     results.textContent = wordResult;
 }
